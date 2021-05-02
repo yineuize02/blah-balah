e5a0b9d4666a1b06716c9c3c31de92e6848122dd
yineuize02 <ezeroone@163.com>
2021-05-02 22:32:34 +0800

    zipkin

--- blah-gateway/src/main/java/com/fml/blah/gateway/config/GatewayCommandLineRunner.java
+++ blah-gateway/src/main/java/com/fml/blah/gateway/config/GatewayCommandLineRunner.java
@@ -5,18 +5,17 @@ import com.fml.blah.common.constants.RedisConstants;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
-public class ResourceServiceImpl {
-
+@Component
+public class GatewayCommandLineRunner implements CommandLineRunner {
   @Autowired private RedisTemplate redisTemplate;
 
-  @PostConstruct
-  public void initData() {
+  @Override
+  public void run(String... args) throws Exception {
     Map<String, List<String>> resourceRolesMap = new TreeMap<>();
     resourceRolesMap.put("GET_/blah-api/hello", CollUtil.toList("ADMIN"));
     resourceRolesMap.put("GET_/blah-api/user/currentUser", CollUtil.toList("ADMIN", "TEST"));

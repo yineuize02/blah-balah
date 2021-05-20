51795228959ac427f4d427995563412793bc2f14
yineuize02 <ezeroone@163.com>
2021-05-20 21:19:25 +0800

    dockerfile

---
+++ blah-user/src/main/java/com/fml/blah/user/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.fml.blah.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+  @GetMapping("/hello")
+  public String helloWorld() {
+    return "hello world";
+  }
+}
